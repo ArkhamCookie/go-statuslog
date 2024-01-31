690b4e086746ac42f92f38da918dba5f7b1927b0
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-01-31 16:02:36 -0600

    Created list function for statuses

---
+++ internal/statuslog/list.go
@@ -0,0 +1,38 @@
+package statuslog
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func List(address string) (*StatuslogListData, error) {
+	if address == "" {
+		return nil, errors.New("no address given")
+	}
+
+	target := fmt.Sprintf("https://api.omg.lol/address/%s/statuses/", address)
+	resp, err := http.Get(target)
+	if err != nil {
+		errorMsg := fmt.Sprintf("could not create GET request: %s", err)
+		return nil, errors.New(errorMsg)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result StatuslogListData
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, errors.New("could not unmarshal JSON")
+	}
+
+	if result.Request.StatusCode != 200 {
+		errorMsg := fmt.Sprintf("status code: %d", result.Request.StatusCode)
+		return nil, errors.New(errorMsg)
+	}
+	return &result, nil
+}
