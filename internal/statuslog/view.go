e6b71dc315c821034b6de022ea1237b8d237e53f
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-02-02 17:32:08 -0600

    Add StatusGet() for fetching a status

---
+++ internal/statuslog/view.go
@@ -0,0 +1,39 @@
+package statuslog
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func StatusGet(address, status string) (*StatuslogRetrieveData, error) {
+	if address == "" {
+		return nil, errors.New("no address given")
+	}
+
+	target := fmt.Sprintf("https://api.omg.lol/address/%s/statuses/%s", address, status)
+	resp, err := http.Get(target)
+	if err != nil {
+		errorMsg := fmt.Sprintf("could not create GET request: %s", err)
+		return nil, errors.New(errorMsg)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result StatuslogRetrieveData
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, errors.New("could not unmarshal JSON")
+	}
+
+	if result.Request.StatusCode != 200 {
+		errorMsg := fmt.Sprintf("status code: %d", result.Request.StatusCode)
+		return &result, errors.New(errorMsg)
+	}
+
+	return &result, nil
+}
