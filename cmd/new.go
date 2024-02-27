828ceb09b8cedb07a12a915b240e9d0a67bc2245
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-02-27 14:19:13 -0600

    Create command and add it to root

---
+++ cmd/new.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"internal/statuslog"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var NewStatusCmd = &cobra.Command{
+	Use: "new <staus>",
+	Short: "Create a new (one line) status",
+	Args: cobra.ExactArgs(1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		success, err := statuslog.NewStatus(args[0])
+		if err != nil {
+			log.Fatalln(err)
+		} else if !success {
+			log.Fatalln("Failed to post status")
+		}
+	},
+}
