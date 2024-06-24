45430ded77e7711f453f19717dfc51f9ca41fab5
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-06-23 22:47:48 -0500

    Feat: Simplify latest command
    Instead of its own internal file,
    just use get all and format it in the command itself.

--- cmd/latest.go
+++ cmd/latest.go
@@ -5,6 +5,7 @@ import (
 	"internal/statuslog"
 	"os"
 
+	"github.com/charmbracelet/glamour"
 	"github.com/spf13/cobra"
 )
 
@@ -14,14 +15,33 @@ var LatestCmd = &cobra.Command{
 	Args: cobra.ExactArgs(1),
 
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get address from args
 		address := args[0]
 
-		result, err := statuslog.LatestGet(address)
+		// Retreive statuses
+		result, err := statuslog.ListGet(address)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		fmt.Sprintln(result)
+		// Format output to print only the latest status
+		output := fmt.Sprintf(
+			"%s %s %s\n",
+
+			result.Response.Statuses[0].Emoji,
+			result.Response.Statuses[0].Content,
+			result.Response.Statuses[0].RelativeTime,
+		)
+
+		// Use glamour to render output
+		out, err := glamour.Render(output, "dark")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Print rendered output
+		fmt.Println(out)
 	},
 }
